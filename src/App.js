0a36e4e7a39442236530ea878f39b8d3af0dce1b
Saloni <suparekh01@gmail.com>
2020-04-27 21:01:11 +0530

    Cards constructed and data rendered from api

--- src/App.js
+++ src/App.js
@@ -14,17 +14,24 @@ import { fetchData } from './api';    //whenever there is index file, you don't 
 
 class App extends React.Component {
 
-    async componentDidMount() {                    //exceptional place for async in case of DidMount function i.e in front of it
-        const modifiedData = await fetchData();
+    state = {
+        data: {},
+    }
 
-        console.log(modifiedData);
+    async componentDidMount() {                            //exceptional place for async in case of DidMount function i.e in front of it
+        const fetchedData = await fetchData();
+
+        //console.log(fetchedData);
+        this.setState( {data: fetchedData});              //We need to render data in cards component so set state
     }
 
 
     render() {
+        const { data } = this.state;                      //{data} will go as props to the card component
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data ={ data } />
                 <Chart />
                 <CountryPicker />
             </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,67 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
+
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+
+    if (!confirmed) {
+        return 'Loading...';
+    }
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">
+                            <CountUp                        // For the jumping numbers
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>                {/*To make the date in a readable form */}
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">
+                            <CountUp                        // For the jumping numbers
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of recoveries of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">
+                            <CountUp                        // For the jumping numbers
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+
+        </div>
     )
 }
 
